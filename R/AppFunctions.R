1ea45aed1c28480b44babc281f3d28ceeeb43d97
Giappo <glaudanno@gmail.com>
2025-03-08 01:16:16 +0100

    Improved DeployApp function

--- R/AppFunctions.R
+++ R/AppFunctions.R
@@ -1,6 +1,8 @@
 #' @export
 DeployApp <- function() {
+  detach("package:PortfolioMontecarlo", unload = TRUE)
   devtools::install_github("Giappo/PortfolioMontecarlo", force = TRUE)
+  library(PortfolioMontecarlo)
   rsconnect::deployApp("inst/shinyApp", appName = "PortfolioMontecarlo")
 }
 
